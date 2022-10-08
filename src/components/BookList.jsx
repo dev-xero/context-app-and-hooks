ae3c80d49999f531bbc9239b3c3667344d8b4a00
CodeNinja-tech <elo48858@gmail.com>
2022-10-08 23:06:47 +0100

    Used Context API in BookList

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
 
 const BookList = () => {
-	return (
-		<div className="book-list">
-			<ul>
-				<li>The Way of Kings</li>
-				<li>The Name of the Wind</li>
-				<li>The Final Empire</li>
-			</ul>
-		</div>
-	)
+  const { isLightTheme, light, dark } = useContext(ThemeContext)
+  const theme = isLightTheme ? light : dark
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, backgroundColor: theme.bg }}
+    >
+      <ul>
+        <li style={{ backgroundColor: theme.ui }}>The Way of Kings</li>
+        <li style={{ backgroundColor: theme.ui }}>The Name of the Wind</li>
+        <li style={{ backgroundColor: theme.ui }}>The Final Empire</li>
+      </ul>
+    </div>
+  )
 }
 
 export default BookList
