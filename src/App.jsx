e4b90b275f48b4e4fcdf22b69fb85c4ebcd9aeeb
CodeNinja-tech <elo48858@gmail.com>
2022-10-08 20:09:01 +0100

    Updated Index CSS File

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar";
+import BookList from "./components/BookList";
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
+      <BookList />
     </div>
   );
 }
