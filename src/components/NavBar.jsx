b5db955c6e13ab4a2c760eaee717707303a46745
CodeNinja-tech <elo48858@gmail.com>
2022-10-08 22:59:15 +0100

    Added Theme Feature to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
 
 const NavBar = () => {
-	return (
-		<nav>
-			<h1>Context App</h1>
-			<ul>
-				<li>Home</li>
-				<li>About</li>
-				<li>Contact</li>
-			</ul>
-		</nav>
-	)
+  const { isLightTheme, light, dark } = useContext(ThemeContext)
+  const theme = isLightTheme ? light : dark
+  return (
+    <nav style={{ backgroundColor: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
 }
 
 export default NavBar
