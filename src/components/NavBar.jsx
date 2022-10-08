450194133de8b0fd1a8211ee0705e7d3917c1e96
CodeNinja-tech <elo48858@gmail.com>
2022-10-08 20:01:01 +0100

    Created the NavBar Component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NavBar() {
+	return (
+		<nav>
+			<h1>Context App</h1>
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+	)
+}
